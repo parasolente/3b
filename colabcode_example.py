34660fde5d06b5a19455126bfa2b00eed3c296e1
parasolente <162950553+parasolente@users.noreply.github.com>
2024-06-24 14:50:54 -0700

    Create colabcode_example.py

---
+++ colabcode_example.py
@@ -0,0 +1,6 @@
+# colabcode_example.py
+
+from colabcode import ColabCode
+
+# Crea una instancia de ColabCode en el puerto 10000 con la contraseña 'password'
+ColabCode(port=10000, password='password')
